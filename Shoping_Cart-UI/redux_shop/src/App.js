ef7a01723a485a0a10eb082d16e165ddc09f4539
vaibhav dhiman <princedhiman70@gmail.com>
2020-09-06 20:01:12 +0530

    setting initialstore, store , reducers

--- Shoping_Cart-UI/redux_shop/src/App.js
+++ Shoping_Cart-UI/redux_shop/src/App.js
@@ -6,12 +6,36 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
+// store -> single source of truth
+// reducer -> function that used to update store
+// dispatch -> send actions to the store
+
+import {createStore} from 'redux';
+
+// initial state value
+const initialStore = {
+  count: 78
+};
+
+// reducer here will dispatch action
+function reducer(state,action) {
+if(action.type === "DECREASE") {
+    // state.count = state.count -1;
+    return {count:state.count-1};
+}
+  return state;
+}
+
+/// store creating
+const store = createStore(reducer,initialStore);
+store.dispatch({type:"DECREASE"});
+console.log(store.getState());
+
 function App() {
   // cart setup
-
   return (
     <div>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </div>
   );
