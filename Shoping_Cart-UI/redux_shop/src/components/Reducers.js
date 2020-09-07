4d99d84ed5279c7421bb72664355c4cb0d0c6a15
vaibhav dhiman <princedhiman70@gmail.com>
2020-09-07 16:21:11 +0530

    increse,decrese done

--- Shoping_Cart-UI/redux_shop/src/components/Reducers.js
+++ Shoping_Cart-UI/redux_shop/src/components/Reducers.js
@@ -7,15 +7,36 @@ function reducer(state,action) {
   }
 
   if(action.type === DECREASE) {
-    return {...state,cart:state.cart -1};
+    let tempCart = [];
+    if(action.payload.amount === 1) {
+      return {...state,
+        cart:state.cart.filter(item=> item.id !== action.payload.id)}
+    }
+    else {
+      let tempCart = state.cart.map((cartItem) => {
+        if(cartItem.id === action.payload.id) {
+          cartItem = {...cartItem,amount:cartItem.amount -1}
+        }
+        return cartItem;
+      })
+      return {...state,cart:tempCart}
+    }
   }
 
   if(action.type === INCREASE) {
-    return {...state,cart:state.cart +1};
+    let tempCart = state.cart.map((cartItem) => {
+      if(cartItem.id === action.payload.id) {
+        cartItem = {...cartItem,amount:cartItem.amount +1}
+      }
+      return cartItem;
+    })
+
+    return {...state,cart:tempCart}
   }
 
   if(action.type === REMOVE) {
-    return {...state,cart:state.cart.filter(item=> item.id !== action.payload.id)}
+    return {...state,
+      cart:state.cart.filter(item=> item.id !== action.payload.id)}
   }
    return state;
 
