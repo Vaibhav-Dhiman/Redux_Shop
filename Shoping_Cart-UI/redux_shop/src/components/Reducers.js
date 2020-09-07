8f70114b5890497ebcebd722d1e3a129014cfd9e
vaibhav dhiman <princedhiman70@gmail.com>
2020-09-07 14:45:50 +0530

    remove item done

--- Shoping_Cart-UI/redux_shop/src/components/Reducers.js
+++ Shoping_Cart-UI/redux_shop/src/components/Reducers.js
@@ -1,6 +1,30 @@
+import {DECREASE,INCREASE,CLEAR_CART,REMOVE} from "./Actions";
+
 // reducer here will dispatch action
 function reducer(state,action) {
-    return state;
+  if(action.type === CLEAR_CART) {
+    return {...state,cart:[]};
+  }
+
+  if(action.type === DECREASE) {
+    return {...state,cart:state.cart -1};
+  }
+
+  if(action.type === INCREASE) {
+    return {...state,cart:state.cart +1};
+  }
+
+  if(action.type === REMOVE) {
+    return {...state,cart:state.cart.filter(item=> item.id !== action.payload.id)}
+  }
+   return state;
+
+      // switch(action.type) {
+  //   case CLEAR_CART:
+  //       return {...state,cart:[],amount:0}
+  //   default:
+  //     return state;
+  // }
   }
 
   export default reducer;
